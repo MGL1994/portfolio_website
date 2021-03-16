2995e8f744a206f31f22c3b0e2200fb8b5ea1bb9
MGL1994 <michaelgl1994@gmail.com>
2021-03-16 21:32:22 +0000

    Added logo animation

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ const triangleContainer = document.querySelector('#triangleContainer')
 const largeTriangleHollow = document.querySelector('#largeTriangleHollow')
 const largeTriangleSolid = document.querySelector('#largeTriangleSolid')
 
+const socialLogos = document.querySelectorAll('i')
+
 navigationGroup.addEventListener('mouseenter', expandBurgerMenu);
 navigationGroup.addEventListener('mouseleave', collapseBurgerMenu);
 
 largeTriangleHollow.addEventListener('mouseenter', showLargeTriangleSolid)
 largeTriangleHollow.addEventListener('mouseleave', hideLargeTriangleSolid)
+
+socialLogos.forEach(logo => {
+    logo.addEventListener('mouseenter', showLogoShadow)
+    logo.addEventListener('mouseleave', hideLogoShadow)
+})
 
 function expandBurgerMenu(e) {
     navigationGroup.classList.add('open')
@@ -27,4 +34,18 @@ function showLargeTriangleSolid() {
 
 function hideLargeTriangleSolid() {
     triangleContainer.classList.remove('show')
+}
+
+function showLogoShadow(e) {
+    const target = e.target.id.split('L')
+    const logo = target[0]
+    const shadow = document.querySelector(`#${logo}Shadow`)
+    shadow.classList.add('show')
+}
+
+function hideLogoShadow(e) {
+    const target = e.target.id.split('L')
+    const logo = target[0]
+    const shadow = document.querySelector(`#${logo}Shadow`)
+    shadow.classList.remove('show')
 }
